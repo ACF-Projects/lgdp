43f80c0019b5b8ce80d4527a01b16a6dd8a08729
izz13 <94653804+izz13@users.noreply.github.com>
2024-06-02 14:43:34 -0700

    updated enemy prefab

---
+++ Assets/_Izzy/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LGDP
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        [SerializeField]
+        List<Transform> pathPoints;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
