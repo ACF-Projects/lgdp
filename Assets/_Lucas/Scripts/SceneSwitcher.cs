1a05f9536845c7cb7e0d3d53da53ae8693395188
LWCoding <DiamondMob01@gmail.com>
2024-07-29 14:07:22 -0700

    Added scene switcher script and UI button prefab

---
+++ Assets/_Lucas/Scripts/SceneSwitcher.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LGDP
+{
+    public class SceneSwitcher : MonoBehaviour
+    {
+
+        public static SceneSwitcher Instance;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(this);
+            }
+            Instance = this;
+        }
+
+        public void GoToScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+       
+    }
+}
---
+++ Assets/_Lucas/Scripts/UILevelHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LGDP
+{
+    public class UILevelHandler : MonoBehaviour
+    {
+
+        [Header("Level Properties")]
+        public string LevelSceneName;
+        
+        /// <summary>
+        /// Go to a specified scene, finding a SceneSwitcher
+        /// singleton if one exists.
+        /// </summary>
+        public void GoToScene()
+        {
+            if (SceneSwitcher.Instance == null)
+            {
+                Debug.LogError("No SceneSwitcher object found!", this);
+                return;
+            }
+            SceneSwitcher.Instance.GoToScene(LevelSceneName);
+        }
+
+    }
+}
