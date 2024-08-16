20d4c248b8b450917c5a6a50f7f9e68a0576eedd
AndyMcGamer <andymcgamer@gmail.com>
2024-08-15 22:17:57 -0700

    camera panning done

--- Assets/_Andy/TowerDefense/Scripts/CameraPanning.cs
+++ Assets/_Andy/TowerDefense/Scripts/CameraPanning.cs
@@ -16,11 +16,14 @@ namespace RushHour
 
         [Header("Settings")]
         [SerializeField] private float panSpeed;
-        
+        [SerializeField] private float panBorderWidth;
+
+        private Vector2 direction;
 
         private void Awake()
         {
             followTarget = vCam.Follow;
+            direction = Vector2.zero;
         }
 
         private void OnEnable()
@@ -33,13 +36,43 @@ namespace RushHour
             MouseReceiver.OnMouseMoved -= TryPanCamera;
         }
 
-        private void TryPanCamera(Vector2 pos)
+        private void Update()
         {
-            if (EventSystem.current.IsPointerOverGameObject()) return;
+            if (direction.sqrMagnitude == 0) return;
 
-            print(pos);
+            followTarget.position += panSpeed * Time.deltaTime * (Vector3)direction;
         }
 
+        private void TryPanCamera(Vector2 pos)
+        {
+            direction = Vector2.zero;
+
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+
+            GetPanDirection(pos.x, pos.y);
+        }
+
+        private void GetPanDirection(float x, float y)
+        {
+            if(y >= Screen.height - panBorderWidth)
+            {
+                direction.y += 1;
+            }
+            else if(y <= panBorderWidth)
+            {
+                direction.y -= 1;
+            }
+
+            if(x >= Screen.width - panBorderWidth)
+            {
+                direction.x += 1;
+            }
+            else if(x <= panBorderWidth)
+            {
+                direction.x -= 1;
+            }
+
+        }
         
     }
 }
