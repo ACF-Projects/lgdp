ecbfd2e2e60226d29e24482b4a717422e091fb57
LWCoding <DiamondMob01@gmail.com>
2024-08-29 18:45:08 -0700

    Added singleton AudioManager

---
+++ Assets/_Lucas/Scripts/AudioManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RushHour
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioManager : MonoBehaviour
+    {
+
+        private static AudioManager _instance;
+        public static AudioManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<AudioManager>();
+                }
+                return _instance;
+            }
+        }
+
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            if (Instance != this)
+            {
+                Destroy(this);
+            }
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        /// <summary>
+        /// Plays a specified audio clip at the specified volume.
+        /// Can be overlayed with other sounds.
+        /// </summary>
+        public void PlayOneShot(AudioClip sfx, float volume = 1)
+        {
+            _audioSource.PlayOneShot(sfx, volume);
+        }
+
+    }
+}
