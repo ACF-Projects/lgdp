f69d3e0f8523c6b894210e7f443b42c5f0b6559c
LWCoding <DiamondMob01@gmail.com>
2024-08-30 17:48:43 -0700

    TowerStore now sorts by cost, added new Grandma customer and Celebrity unit, revamped sprite for Intern

---
+++ Assets/_Lucas/Scripts/TowerStore.cs
@@ -0,0 +1,55 @@
+using RushHour.Data;
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Lucas
+{
+    public class TowerStore : MonoBehaviour
+    {
+
+        public static TowerStore Instance;
+
+        public List<TowerData> AllTowerData;
+
+        #region Singleton Logic
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(this);
+            }
+            Instance = this;
+        }
+        #endregion
+
+#if UNITY_EDITOR
+        /// <summary>
+        /// Searches through the entire project for files of type "TowerData".
+        /// Then, clears and adds all these to the `AllTowerData` list.
+        /// </summary>
+        [Button]
+        public void RefreshTowerData()
+        {
+            AllTowerData = new List<TowerData>();
+            string[] guids = AssetDatabase.FindAssets("t:TowerData");
+
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                TowerData obj = AssetDatabase.LoadAssetAtPath<TowerData>(path);
+                if (obj != null)
+                {
+                    AllTowerData.Add(obj);
+                }
+            }
+
+            AllTowerData.Sort((a, b) => (int)a.Cost - (int)b.Cost);  // Sort by cost
+        }
+#endif
+
+    }
+}
