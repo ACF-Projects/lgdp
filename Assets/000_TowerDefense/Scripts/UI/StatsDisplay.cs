cd34e0661a38547f41a0e4d2b83495e45de8866b
AndyMcGamer <andymcgamer@gmail.com>
2024-10-01 18:33:46 -0700

    fixed camera collision issues

--- Assets/000_TowerDefense/Scripts/UI/StatsDisplay.cs
+++ Assets/000_TowerDefense/Scripts/UI/StatsDisplay.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace RushHour.UserInterface
 {
+    [System.Serializable]
+    public class StatSlot<T>
+    {
+        public TextMeshProUGUI nameField;
+        public T valueField;
+    }
+
     public class StatsDisplay : MonoBehaviour
     {
-        
+        //[SerializeField] private StatSlot[] slots;
     }
 }
