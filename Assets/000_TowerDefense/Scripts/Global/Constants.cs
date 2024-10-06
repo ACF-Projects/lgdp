99da61f58523a9323d25d5b4f31573f7bc7eba12
LWCoding <DiamondMob01@gmail.com>
2024-10-06 01:37:08 -0700

    Increased time of day (2 hours -> 6 hours)

--- Assets/000_TowerDefense/Scripts/Global/Constants.cs
+++ Assets/000_TowerDefense/Scripts/Global/Constants.cs
@@ -12,7 +12,7 @@ namespace RushHour.Global
 
         public static readonly LayerMask LAYERS_ALL_EXCEPT_CAMERA = ~(1 << LayerMask.NameToLayer("Camera"));
 
-        public const int TIME_IN_DAY = 20;  // Number of seconds in a day until day ends
+        public const int TIME_IN_DAY = 60;  // Number of seconds in a day until day ends
         public const int TIME_IN_HOUR = 10;  // How many seconds must pass for an in-game hour
     }
 }
