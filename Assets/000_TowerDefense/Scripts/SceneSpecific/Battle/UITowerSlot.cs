c016a7d98283f41627ae98d085cdd76edd301f93
LWCoding <DiamondMob01@gmail.com>
2024-11-03 16:27:27 -0800

    Added salary information to unit info

--- Assets/000_TowerDefense/Scripts/SceneSpecific/Battle/UITowerSlot.cs
+++ Assets/000_TowerDefense/Scripts/SceneSpecific/Battle/UITowerSlot.cs
@@ -1,7 +1,5 @@
 using RushHour.Data;
 using RushHour;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -26,7 +24,7 @@ namespace RushHour.UserInterface
 
         public void Initialize(TowerData data)
         {
-            _costText.text = "$" + data.Cost.ToString();
+            _costText.text = "<size=21>Cost: <color=#159614>$" + data.Cost.ToString() + "</color></size>\n(<color=\"red\">-$" + data.SalaryPerHour.ToString() + "/hr</color>)";
             _slotImage.sprite = data.UIIcon;
             _cachedData = data;
             DisableDragIfTooPoor();
@@ -70,80 +68,7 @@ namespace RushHour.UserInterface
             // Create world object, disable its functionality, and bring it to the mouse
             TowerHandler obj = Instantiate(_cachedData.towerPrefab, worldPosition, Quaternion.identity).GetComponent<TowerHandler>();  // Spawn tower
             obj.Init(_cachedData);
-
-            //obj.ShowEffectRadius();
-            //obj.IsActivated = false;
-            //_spawnedTowerHandler = obj;
         }
-
-        /// <summary>
-        /// When we are dragging, make the tower follow the mouse and update its
-        /// visuals based on placeability.
-        /// </summary>
-        //public void Update()
-        //{
-        //    if (_spawnedTowerHandler != null)
-        //    {
-        //        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //        worldPosition.z = 0;
-
-        //        _spawnedTowerHandler.transform.position = worldPosition;  // Tower follows mouse
-
-        //        // Collision check with blocked objects or UI; check for 1 collision (with itself)
-        //        Collider2D[] col = Physics2D.OverlapCircleAll(worldPosition, _cachedData.SpriteRadius, LayerMask.GetMask("Blocked", "Obstacle"));
-        //        _isPlaceable = col.Length == 1 && EventSystem.current.currentSelectedGameObject == null;
-
-        //        if (_isPlaceable)
-        //        {
-        //            _spawnedTowerHandler.TintEffectRadius(Color.white);
-        //        }
-        //        else
-        //        {
-        //            _spawnedTowerHandler.TintEffectRadius(Color.red);
-        //        }
-        //    }
-        //}
-
-        /// <summary>
-        /// When a drag end, attempt to place currently selected tower at the mouse.
-        /// </summary>
-        //public void OnPointerUp(PointerEventData eventData)
-        //{
-        //    if (_spawnedTowerHandler == null) { return; }
-        //    ContextManager.instance.ChangeContext(ContextType.Shop);  // Re-show menu after dragging
-
-        //    // If we are hovering over an invalid spot, then we cannot place
-        //    if (!_isPlaceable)
-        //    {
-        //        Destroy(_spawnedTowerHandler.gameObject);
-        //        _spawnedTowerHandler = null;
-        //        AudioManager.Instance.PlayOneShot(SoundEffect.InvalidPlacement);
-        //        return;
-        //    }
-
-        //    // If we don't have sufficient funds, then we also cannot place
-        //    if (BattleManager.Instance.Money < _cachedData.Cost)
-        //    {
-        //        Destroy(_spawnedTowerHandler.gameObject);
-        //        _spawnedTowerHandler = null;
-        //        AudioManager.Instance.PlayOneShot(SoundEffect.InsufficientFunds);
-        //        return;
-        //    }
-
-        //    _spawnedTowerHandler.IsActivated = true;  // Activate the tower after its placed
-        //    _spawnedTowerHandler = null;
-
-        //    BattleManager.Instance.Money -= _cachedData.Cost;  // Decrease current funds
-        //    BattleManager.Instance.SalaryPerHour += _cachedData.SalaryPerHour;
-
-        //    AudioManager.Instance.PlayOneShot(_placedUnitSFX, 0.6f);  // Play sound effect when placed
-
-        //    // If the unit has a unique placement sound effect, also play that too
-        //    if (_cachedData.PlacementSFX != null)
-        //    {
-        //        AudioManager.Instance.PlayOneShot(_cachedData.PlacementSFX, _cachedData.PlacementSFXVolume);
-        //    }
-        //}
 
     }
 }
